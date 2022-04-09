34193199ce0132272c03e5d1b25491c0aef2df27
Abgold11 <103322187+Abgold11@users.noreply.github.com>
2022-04-09 17:36:32 +0300

    Uploaded files
    Uploaded from codesandbox

---
+++ script.js
@@ -0,0 +1,96 @@
+let currentTime = new Date();
+function setDate(date) {
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  let currentDate = date.getDate();
+  let currentMonth = months[date.getMonth()];
+  let hour = currentTime.getHours();
+  let hour_string = `${hour}`;
+  if (hour < 10) {
+    hour_string = "0" + hour_string;
+  }
+  let minutes = currentTime.getMinutes();
+  let minutes_string = `${minutes}`;
+  if (minutes < 10) {
+    minutes_string = "0" + minutes_string;
+  }
+  //return `${currentMonth} ${currentDate} ${hour}:${minutes}`
+
+  let nowTime = document.querySelector("#nowTime");
+  nowTime.innerHTML = `${hour_string}:${minutes_string}`;
+  let nowDate = document.querySelector("#nowDate");
+  nowDate.innerHTML = `${currentDate} ${currentMonth}`;
+}
+
+setDate(currentTime);
+
+function celciusF(event) {
+  event.preventDefault();
+  let fahrenheitTemp = document.querySelector(".current");
+  let tempF = Math.round((15 * 9) / 5 + 32);
+  fahrenheitTemp.innerHTML = tempF;
+}
+
+function fahrenheitC(event) {
+  event.preventDefault();
+  let celciusTemp = document.querySelector(".current");
+  let tempC = Math.round(((59 - 32) * 5) / 9);
+  celciusTemp.innerHTML = tempC;
+}
+
+let fahrenheit = document.querySelector(".fahrenheit");
+fahrenheit.addEventListener("click", celciusF);
+let convert = document.querySelector(".celcius");
+convert.addEventListener("click", fahrenheitC);
+
+function displayWeather(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#current").innerHTML = Math.round(
+    response.data.main.temp
+  );
+}
+
+function searchCity(city) {
+  let apiKey = "ca8471e6e6da36ee717fbd8bc9f904e0";
+  let apiSite = "https://api.openweathermap.org/data/2.5/weather";
+  let apiUrl = `${apiSite}?q=${city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function search(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+//console.log(input.value);
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
+
+function geoPosition(response) {
+  let longitude = response.coords.longitude;
+  let latitude = response.coords.latitude;
+  let apiKey = "ca8471e6e6da36ee717fbd8bc9f904e0";
+  let geoUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+  axios.get(geoUrl).then(displayWeather);
+}
+
+function getPosition(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(geoPosition);
+}
+
+let currentLocation = document.querySelector("#currentLocation");
+currentLocation.addEventListener("click", getPosition);
